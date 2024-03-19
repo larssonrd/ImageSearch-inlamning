1f30e3ec8a26fafa59386ba7bb25ad1e6a418c0b
larssonrd <94178885+larssonrd@users.noreply.github.com>
2024-03-19 17:33:58 +0100

    Route fix

--- client/src/pages/Layout.tsx
+++ client/src/pages/Layout.tsx
@@ -1,14 +1,14 @@
 import { Outlet } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import { useAuth0 } from '@auth0/auth0-react';
 
 const Layout = () => {
+  const { isAuthenticated } = useAuth0();
   return (
     <div className='flex flex-col min-h-screen'>
       <Header />
-      <main className='flex-1'>
-        <Outlet />
-      </main>
+      <main className='flex-1'>{isAuthenticated && <Outlet />}</main>
       <Footer />
     </div>
   );
